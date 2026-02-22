be5635dbada06bd4852468209c797e6ff8bb206e
AlexandraSpeshilova <spesilovasasa13@gmail.com>
2026-02-22 21:15:00 +0500

    восстановление пароля

--- security.permaviat.ru/ajax/recovery.php
+++ security.permaviat.ru/ajax/recovery.php
@@ -1,46 +1,50 @@
 <?php
-	session_start();
-	include("../settings/connect_datebase.php");
-	
-	$login = $_POST['login'];
-	
-	// ищем пользователя
-	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."';");
-	
-	$id = -1;
-	if($user_read = $query_user->fetch_row()) {
-		// создаём новый пароль
-		$id = $user_read[0];
-	}
-	
-	function PasswordGeneration() {
-		// создаём пароль
-		$chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; // матрица
-		$max=10; // количество
-		$size=StrLen($chars)-1; // Определяем количество символов в $chars
-		$password="";
-		
-		while($max--) {
-			$password.=$chars[rand(0,$size)];
-		}
-		
-		return $password;
-	}
-	
-	if($id != 0) {
-		//обновляем пароль
-		$password = PasswordGeneration();;
-		// проверяем не используется ли пароль 
-		$query_password = $mysqli->query("SELECT * FROM `users` WHERE `password`= '".md5($password)."';");
-		while($password_read = $query_password->fetch_row()) {
-			// создаём новый пароль
-			$password = PasswordGeneration();
-		}
-		// обновляем пароль
-		$mysqli->query("UPDATE `users` SET `password`='".md5($password)."' WHERE `login` = '".$login."'");
-		// отсылаем на почту
-		//mail($login, 'Безопасность web-приложений КГАПОУ "Авиатехникум"', "Ваш пароль был только что изменён. Новый пароль: ".$password);
-	}
-	
-	echo $id;
+  session_start();
+  include("../settings/connect_datebase.php");
+  
+  $login = $_POST['login'];
+  
+  // ищем пользователя
+  $query_user = $mysqli->query("SELECT * FROM users WHERE login='".$login."';");
+  
+  $id = -1;
+  if($user_read = $query_user->fetch_row()) {
+    // создаём новый пароль
+    $id = $user_read[0];
+  }
+  
+  function PasswordGeneration() {
+    // создаём пароль
+    $chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; // матрица
+    $max=10; // количество
+    $size=StrLen($chars)-1; // Определяем количество символов в $chars
+    $password="";
+    
+    while($max--) {
+      $password.=$chars[rand(0,$size)];
+    }
+    
+    return $password;
+  }
+  
+  if($id != 0) {
+  $DateStart = date('Y-m-d H:i:s');
+  $Ip = $_SERVER['REMOTE_ADDR'];
+    //обновляем пароль
+    $password = PasswordGeneration();;
+    // проверяем не используется ли пароль 
+    $query_password = $mysqli->query("SELECT * FROM users WHERE password= '".md5($password)."';");
+    while($password_read = $query_password->fetch_row()) {
+      // создаём новый пароль
+      $password = PasswordGeneration();
+    }
+    // обновляем пароль
+    $mysqli->query("UPDATE users SET password='".md5($password)."' WHERE login = '".$login."'");
+    // отсылаем на почту
+    //mail($login, 'Безопасность web-приложений КГАПОУ "Авиатехникум"', "Ваш пароль был только что изменён. Новый пароль: ".$password);
+  }
+    $Sql = "INSERT INTO logs (Ip, IdUser, Date, TimeOnline, Event)
+        VALUES ('{$Ip}', '{$user_read[0]}', '{$DateStart}', '00:00:00', 'Пользователь {$login} восстановил пароль')";
+      $mysqli->query($Sql);
+  echo $id;
 ?>
